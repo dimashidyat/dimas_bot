f61ef6ee16ea4a2605dbe3249f0c52d797c9c95a
dimashidyat <77518929+dimashidyat@users.noreply.github.com>
2025-01-27 08:28:12 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 import os
 
-TOKEN = os.getenv("TOKEN")  # Railway akan ambil token dari environment variables
+# Token API dari BotFather
+TOKEN = os.getenv("TOKEN")  # Masukkan token sebagai environment variable
